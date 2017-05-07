f041fee7b7d80a9a2f2f794ea0ea8d7048e235c3
Alex M <aljmiller87@gmail.com>
2017-05-07 13:46:32 -0400

    changed to Controller As syntax

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 angular.module('myApp', []).
-controller('myCtrl', function($scope) {
-	let femaleName = '';
-	let jobTitle = '';
-	let tediousTask = '';
-	let dirtyTask = '';
-	let celebrity = '';
-	let uselessSkill = '';
-	let adjective = '';
-	let obnoxiousCelebrity = '';
-	let hugeNumber = '';
+// controller('myCtrl', function($scope) {
+// 	let femaleName = '';
+// 	let jobTitle = '';
+// 	let tediousTask = '';
+// 	let dirtyTask = '';
+// 	let celebrity = '';
+// 	let uselessSkill = '';
+// 	let adjective = '';
+// 	let obnoxiousCelebrity = '';
+// 	let hugeNumber = '';
 
-	$scope.femaleName = femaleName;
-	$scope.jobTitle = jobTitle;
-	$scope.tediousTask = tediousTask;
-	$scope.dirtyTask = dirtyTask;
-	$scope.celebrity = celebrity;
-	$scope.uselessSkill = uselessSkill;
-	$scope.adjective = adjective;
-	$scope.obnoxiousCelebrity = obnoxiousCelebrity;
-	$scope.hugeNumber = hugeNumber;
+// 	$scope.femaleName = femaleName;
+// 	$scope.jobTitle = jobTitle;
+// 	$scope.tediousTask = tediousTask;
+// 	$scope.dirtyTask = dirtyTask;
+// 	$scope.celebrity = celebrity;
+// 	$scope.uselessSkill = uselessSkill;
+// 	$scope.adjective = adjective;
+// 	$scope.obnoxiousCelebrity = obnoxiousCelebrity;
+// 	$scope.hugeNumber = hugeNumber;
+// });
+
+controller('myCtrl', function($) {
+	let vm = this;
+	vm.femaleName = '';
+	vm.jobTitle = '';
+	vm.tediousTask = '';
+	vm.dirtyTask = '';
+	vm.celebrity = '';
+	vm.uselessSkill = '';
+	vm.adjective = '';
+	vm.obnoxiousCelebrity = '';
+	vm.hugeNumber = '';
 });
